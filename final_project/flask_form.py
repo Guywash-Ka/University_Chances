5ea535049381f86da2526bc37a4459b34474e3bd
Guywash-Ka <57402279+Guywash-Ka@users.noreply.github.com>
2020-04-05 21:41:43 +0300

    Add files via upload
    abc

--- final_project/flask_form.py
+++ final_project/flask_form.py
@@ -1,15 +1,42 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me')
-    submit = SubmitField('submit')
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    # recaptcha = RecaptchaField()
+    submit = SubmitField('Войти')
 
 
 class CheckForm(FlaskForm):
-    username = StringField('Ваш балл:', validators=[DataRequired()])
+    score = IntegerField('Ваш балл:', validators=[DataRequired()])
     submit = SubmitField('Проверить')
+    submit_test = SubmitField('Перейти к тесту')
+
+
+class TestForm(FlaskForm):
+    username = StringField('Ответ')
+    submit_next = SubmitField('Далее', description='Next')
+    submit_back = SubmitField('Назад', description='Previous')
+    submit_end = SubmitField('Закончить тест', description='End')
+    submit_to_main = SubmitField('Вернуться назад')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Почта*', validators = [DataRequired()])
+    password = PasswordField('Пароль*', validators = [DataRequired()])
+    password_again = PasswordField('Повторите пароль*', validators = [DataRequired()])
+    surname = StringField('Фамилия*', validators=[DataRequired()])
+    name = StringField('Имя*', validators = [DataRequired()])
+    age = IntegerField('Возраст*', validators = [DataRequired()])
+    about = StringField("О себе")
+    remember_me = BooleanField('Запомнить меня')
+    # recapcha = RecaptchaField()
+    submit = SubmitField('Подтвердить')
+
+
+class SuccessForm(FlaskForm):
+    submit_back = SubmitField('Вернуться назад')
